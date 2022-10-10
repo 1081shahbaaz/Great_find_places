0d1548b3fa2e51b44e96f890b5bab4bc2aa14913
inam220 <115453314+inam220@users.noreply.github.com>
2022-10-10 13:03:54 +0530

    Update place.dart

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -9,6 +9,618 @@ class Place {
   Place(
       {required this.id,
       required this.title,
+       itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}
+    itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,import 'dart:io';
+
+class Place {
+  final String id;
+  final String title;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+       itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}
+    itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}
+
+import 'dart:io';
+
+class Place {
+  final String id;
+  final String title;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+       itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}
+    itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}itle;
+  final PlaceLocation? location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}
+
+      required this.title,
+      this.location,
+      required this.image});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  String? addess;
+
+  PlaceLocation({required this.latitude, required this.longitude, this.addess});
+}
       this.location,
       required this.image});
 }
